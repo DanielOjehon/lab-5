6f8cbe2da20aad19e54070a9a67d84c6fc43e310
DanielOjehon <g00466771@atu.ie>
2026-03-09 14:48:37 +0000

    Add methods to fetch student and weather data

--- src/app/Services/data-service.ts
+++ src/app/Services/data-service.ts
@@ -8,9 +8,12 @@ import { Observable } from 'rxjs';
 export class DataService {
   constructor(private httpClient:HttpClient){}
 
+  // Fetches student data from an external API
   GetStudentData():Observable<any>{
     return this.httpClient.get('https://api.jsonblob.com/019caf22-4328-7dce-99d0-a69e7ca16f39')
   }
+
+  // Retrieves current weather data for Galway from OpenWeather API
   GetWeatherData():Observable<any>{
     return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303')
   }
